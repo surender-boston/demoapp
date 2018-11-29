372bf68257595cb853bd2ccf658c32e8f0b974ca
Tushar <tushark@boston-technology.com>
2018-11-29 18:05:51 +0530

    created project

---
+++ Demo-CICD/Demo-CICD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo-CICD
+//
+//  Created by Tushar on 11/29/18.
+//  Copyright © 2018 BTC Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
